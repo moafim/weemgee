f063293a30d8a7e7583f5ad4ee14fd48ff96e0b9
Milad Moafi <milixov@gmail.com>
2019-10-13 21:53:30 +0330

    banner home page

--- src/components/HomeBanner/index.js
+++ src/components/HomeBanner/index.js
@@ -6,14 +6,44 @@ import styles from "./style.module.scss";
 function HomeBanner() {
   return (
     <div className={styles.container}>
-      <div>
-        <Banner />
-        <Banner />
+      <div className={styles.bannerContainer}>
+        <Banner
+          height="277px"
+          ctaHeight="82px"
+          title="Living Room"
+          stat="+2450 Products"
+          gradient="right, #0ba36060, #3cba9260"
+        />
+        <Banner
+          height="277px"
+          ctaHeight="82px"
+          title="Bedroom"
+          stat="+2450 Products"
+          gradient="left, #13547a60, #80d0c760"
+        />
       </div>
-      <div>
-        <Banner />
-        <Banner />
-        <Banner />
+      <div className={styles.bannerContainer}>
+        <Banner
+          height="220px"
+          ctaHeight="58px"
+          title="Furniture"
+          stat="+2450 Products"
+          gradient="bottom, #0c348360, #6b8cce60"
+        />
+        <Banner
+          height="220px"
+          ctaHeight="58px"
+          title="Lightening"
+          stat="+2450 Products"
+          gradient="bottom, #d0969360, #c71d6f60"
+        />
+        <Banner
+          height="220px"
+          ctaHeight="58px"
+          title="Kitchen"
+          stat="+2450 Products"
+          gradient="left, #3d4e8160, #5753c960, #6e7ff360"
+        />
       </div>
     </div>
   );
--- src/components/HomeBanner/item/index.js
+++ src/components/HomeBanner/item/index.js
@@ -2,8 +2,24 @@ import React from "react";
 
 import styles from "./style.module.scss";
 
-function Banner() {
-  return <div className={styles.container}>banner</div>;
+function Banner(props) {
+  const { title, stat, gradient, height, ctaHeight } = props;
+  return (
+    <div
+      className={styles.container}
+      style={{
+        "--height": height || "10px",
+        "--ctaHeight": ctaHeight || "10px",
+        "--gradient": gradient || "red"
+      }}
+    >
+      <div className={styles.imageContainer}></div>
+      <div className={styles.cta}>
+        <span className={styles.title}>{title}</span>
+        <span className={styles.stat}>{stat}</span>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
